ce474e997b4f81f0f2315f7223c94c314a71f4e7
Aditya Bhagat <141161589+Adiiii03@users.noreply.github.com>
2025-04-22 12:57:00 -0700

    Update page_table.cpp

--- page_table.cpp
+++ page_table.cpp
@@ -5,181 +5,182 @@
 // name: Aditya Bhagat
 // RedID: 828612974
 
+#include <iostream>
+#include <unistd.h>
+
+#include "vaddr_tracereader.h"
 #include "page_table.h"
-#include <cmath>
-
-Level* create_level(int depth, int entryCount, PageTable* pageTablePtr) {
-
-  Level* newLevel = new Level;   // create new level
-
-  newLevel->depth = depth;
-  newLevel->pageTablePtr = pageTablePtr;
-
-  newLevel->nextLevelPtr = nullptr;
-  Map* mapPtr = nullptr;
-  // add entry count to page table entries
-  pageTablePtr->pgTableEntries += entryCount;
-
-	// insert page entries; new level array for internal nodes, map objects for leaf nodes
-  if (depth != pageTablePtr->levelCount - 1) {
-    newLevel->nextLevelPtr = new Level*[entryCount];
-    for (int i = 0; i < entryCount; i++) {
-      newLevel->nextLevelPtr[i] = nullptr;
-    }
-  }
-   else {
-     newLevel->mapPtr = new Map[entryCount];
-     for (int i = 0; i < entryCount; i++) {
-       newLevel->mapPtr[i] = create_map();
-     }
-   }
-
-  return newLevel;
-}
-
-Map create_map() {
-    // create new map object to return
-   Map newMap = *new Map;
-   // set default values of map object
-   newMap.frameNum = -1;
-   newMap.valid = false;
+#include "log_helpers.h"
+#include "WSClock.h"
+#include <vector>
 
 
-   return newMap;
-}
+int main(int argc, char* argv[]) {
 
-PageTable* create_pagetable(int bitsPerLevel[], int levelCount) {
+    int numOfAddresses = 0;         // count of addresses processed
+    int frameNum = 0;               // stores physical frame number, which starts at 0 (and will be sequentially incremented)
+    p2AddrTr mtrace;                // structure is typedefed in vaddr_tracereader
+    unsigned int vAddr;             // unsigned 32-bit integer type
 
-  PageTable* newPageTable = new PageTable;  // creating new page table
-  newPageTable->levelCount = levelCount;    // storing number of levels
+    std::vector<WSClockEntry> WSclock;      // vector list of type WSClockEntryto hold the clocks entries
+    int clock_hand_position = 0;            // position of lock hand starting at 0
+    
+    int maxNumMemAccesses = -1;             // only process first N memory accesses, default is all
+    int numPhysicalFrames = 999999;         // number of available physical frames, default is 999999
+    int ageRecentLastAccess = 10;           // age of last access considered recent for page replacement, default is 10
+    const char* logMode = "summary";        // log mode, specifying what to be printed, default is "summary"
 
 
-  // store count of bits for offset; offset = 32 - (number of bits for each level)
-  newPageTable->numOfBitsOffset = 32;
-  for (int i = 0; i < levelCount; i++) {
-    newPageTable->numOfBitsOffset -= bitsPerLevel[i];
-  }
-
-  // create array of bits to shift for getting level i page index
-  newPageTable->shiftAry = new int[newPageTable->levelCount];
-  // create array of bits for bit mask for level i
-  newPageTable->bitMaskAry = new unsigned int[newPageTable->levelCount];
-
-  int shiftAryAmt = 32;               // assuming the address has 32 bits, initialize to 32 and decrement from there
-  unsigned int bit_mask = 0x00000000; // initialize hex value to 0
-  for (int i = 0; i < newPageTable->levelCount; i++) {
-    shiftAryAmt -= bitsPerLevel[i];   // for each level, decrement by # of bits per level to get shift amount
-
-    // get hex value to add to bit mask array
-    bit_mask = (1U << bitsPerLevel[i]) - 1;  // ex: if bitsPerLevel[i] = 5, we need 111111(binary) or 31(decimal)
-    bit_mask <<= shiftAryAmt;                // shift by shift amount given the level i
-    newPageTable->bitMaskAry[i] = bit_mask;  // store hex value into bits mask array
-
-    newPageTable->shiftAry[i] = shiftAryAmt; // store shift amount for each level i
-  }
-
-  // create array of # of next level entries for level i
-  // for each level, the entry count is 2^(number of bits for level i)
-  newPageTable->entryCount = new int[newPageTable->levelCount];
-  int base = 2;                 // base is 2 because two binary values: 0 and 1
-  int exponent = 0;
-  for (int i = 0; i < newPageTable->levelCount; i++) {
-    exponent = bitsPerLevel[i];  // exponent = number of bits for level i
-    newPageTable->entryCount[i] = std::pow(base, exponent);
-  }
-
-  int root_depth = 0;
-  // create root level with depth 0, entry count for the first level (so index 0), page table pointer
-  newPageTable->rootLevel = create_level(root_depth, newPageTable->entryCount[0], newPageTable);
-
-  // additional variable for summary
-  exponent = newPageTable->numOfBitsOffset;
-  newPageTable->pageSize = std::pow(base, exponent);           // bytes per page = 2^(offset)
-
-  // all initialized to 0 because no addresses have been processed, replaced, mapped, allocated
-  newPageTable->numOfPageReplaced = 0;      // number of page replacement
-  newPageTable->pageTableHits = 0;          // number of times a virtual page was mapped
-  newPageTable->numOfAddresses = 0;         // number of addresses processed
-  newPageTable->numOfFramesAllocated = 0;   // number of frames allocated
-  newPageTable->pgTableEntries = 0;         // total number of page table entries across all levels
-
-  return newPageTable;
-}
-
-unsigned int getVPNFromVirtualAddress(unsigned int virtualAdd, unsigned int mask, unsigned int shift) {
-
-  int page;   // stores the VPN of any level or full VPN, depending on given mask or shift amount
-
-  page = virtualAdd & mask;  // extract the relevant bits with bitwise and
-  page >>= shift;            // shift right by given shift amount
-
-  return page;
-}
-
-// Inserting VPN -> PFN mapping to pagetable
-void insertVpn2PfnMapping(PageTable* PageTable, unsigned int VPN, int frameNum) {
-
-    // starting from root level
-    Level* currLevel = PageTable->rootLevel;
-
-    // traversing through each level in pagetable
-    for (int i=0; i < PageTable->levelCount; ++i) {
-
-        int vpn_index = getVPNFromVirtualAddress(VPN, PageTable->bitMaskAry[i], PageTable->shiftAry[i]);
-        if (vpn_index < 0) {
-          return;
-        }
-
-        // when leaf level reached and map PFN does not exist, create map object at given index
-        if (i == PageTable->levelCount - 1) {
-        	if (currLevel->mapPtr[vpn_index].frameNum == -1) {
-            	currLevel->mapPtr[vpn_index].frameNum = frameNum;           // assigning PFN
-            	currLevel->mapPtr[vpn_index].valid = (frameNum >= 0);       // setting validity
-            	PageTable->numOfFramesAllocated++;
-           		return;
+    // command line argument parsing
+    int opt;
+    while ((opt = getopt(argc, argv, "n:f:a:l:")) != -1) {
+        switch (opt) {
+            case 'n':
+                maxNumMemAccesses = atoi(optarg);
+                if (maxNumMemAccesses < 1) {
+                  std::cerr << "Number of memory accesses must be a number, greater than 0" << std::endl;
+                  return 1;    // exit prgram, error occurred
+                  }
+            break;
+            case 'f':
+                numPhysicalFrames = atoi(optarg);
+                if (numPhysicalFrames < 1) {
+                  std::cerr << "Number of available frames must be a number, greater than 0" << std::endl;
+                  return 1;    // exit program, error occurred
                 }
-         }
-         // creating next level nodes for non-leaf level if it doesn't exist
-        else {
-          if (currLevel->nextLevelPtr[vpn_index] == nullptr) {
-            currLevel->nextLevelPtr[vpn_index] = create_level(i+1, PageTable->entryCount[i+1], PageTable);
-          }
-          // moving to next level
-          currLevel = currLevel->nextLevelPtr[vpn_index];
+            break;
+            case 'a':
+                ageRecentLastAccess = atoi(optarg);
+                if (ageRecentLastAccess < 1) {
+                  std::cerr << "Age of last access considered recent must be a number, greater than 0" << std::endl;
+                }
+            break;
+            case 'l':
+                 logMode = optarg;
+            break;
+            default:
+                return 1;
         }
     }
-}
 
-// looks up the VPN in the pagetable and returns the Map only if valid
-Map* findVpn2PfnMapping(PageTable* PageTable, unsigned int VPN) {
-    // starting at the root node
-    Level* currLevel = PageTable->rootLevel;
+    int idx = optind;           // parameter, indexes next element to be processed
 
-    // traversing through levels in pagetable
-    for (int i=0; i < PageTable->levelCount; ++i){
+    const char* mtrace_file_name = argv[idx];
+    idx++;                      // increment index to get next argument -- file
+    const char* readswrites_file_name = argv[idx];
+    idx++;                      // increment index to get next argument -- num bits for level(s)
 
-      	// extracting the index into that level's array
-        int index = getVPNFromVirtualAddress(VPN, PageTable->bitMaskAry[i], PageTable->shiftAry[i]);
-        if (index < 0) {
-          return nullptr;
+    // opening trace and read/writes file
+    FILE* mtrace_file = fopen(mtrace_file_name, "r");
+    FILE* readswrites_file = fopen(readswrites_file_name, "r");
+
+    // checking if file opens correcrtly
+    if (!mtrace_file) {
+        std::cerr << "Unable to open <<trace.tr>>" << std::endl;
+             return 1;          // exit program, error occurred
+    }
+    if (!readswrites_file) {
+        std::cerr << "Unable to open <<readswrites.tr>>" << std::endl;
+        return 1;
+    }
+
+
+    // store number of bits to be used for each level
+    int totalNumBitsAllLevels = 0;    // store total number of bits for all levels
+    int levelCount = argc - idx;
+    int bitsPerLevel[levelCount];    // create array to store bits for each level
+    if (idx < argc) {
+        for (int i = 0; i < levelCount; i++) {
+          bitsPerLevel[i] = atoi(argv[idx + i]);            // converting and storing bits per level
+          totalNumBitsAllLevels += bitsPerLevel[i];
         }
-        
 
-        // if at leaf level and the map object has a valid flag, return the map
-        if (i == PageTable->levelCount - 1){
-            if (!currLevel->mapPtr[index].valid){        // check if the map entry is valid
-            return nullptr;                             // if not return nullptr
+        // checking if too many bits used
+       if (totalNumBitsAllLevels > 28) {
+         std::cerr << "Too many bits used in page tables" << std::endl;
+         return 1;          // exit program, error occurred
+       }
+    }
+    
+    // creating pagetable and WSClock entry arrays
+    PageTable* page_table = create_pagetable(bitsPerLevel, levelCount);
+    WSClockEntry** WSClock_entry = new WSClockEntry*[numPhysicalFrames]();      // array of pointers for WSClock entries
+    
+    // main memory access loop
+    // magic number for maxnumMemAccess!!
+    while ((maxNumMemAccesses == -1 || numOfAddresses < maxNumMemAccesses) && NextAddress(mtrace_file, &mtrace)) {
+        vAddr = mtrace.addr;        // reading & storing virtual address
+
+        // extracting VPNs for each level
+        unsigned int vpns[levelCount];
+        for (int i = 0; i < levelCount; ++i) {
+            vpns[i] = getVPNFromVirtualAddress(vAddr, page_table->bitMaskAry[i], page_table->shiftAry[i]);
+        }
+
+        // checking if access is a write or read
+        bool isWrite = (fgetc(readswrites_file) == '1');
+
+        // checking if the address has existing mapping already
+        Map* map = findVpn2PfnMapping(page_table, vAddr);
+        unsigned int fullVPN = vAddr >> page_table->numOfBitsOffset; // the full VPN for logging
+
+        if (map != nullptr) {                // pagetable hit
+            page_table->pageTableHits++;
+            WSClock_entry[map->frameNum]->lastUsedTime = numOfAddresses;        // updating last used time
+            if (isWrite) {
+                WSClock_entry[map->frameNum]->dirty = true;            // if the page is written, flag it as dirty
             }
-            return &currLevel->mapPtr[index];           // if valid, return the pointer to Map object with PFN
+            if (strcmp(logMode, "vpn2pfn_pr") == 0){
+                log_mapping(fullVPN, map->frameNum, -1, true);     // logging map hit
+            }
+                
+        }
+        else {                               // pagetable miss
+            if (page_table->numOfFramesAllocated < numPhysicalFrames) {         // checking if a free frame is available
+                insertVpn2PfnMapping(page_table, vAddr, frameNum);              // if yes, add new mapping
+                WSClock_entry[frameNum] = create_WSClock_entry(vAddr, frameNum, numOfAddresses, isWrite);
+                if (strcmp(logMode, "vpn2pfn_pr") == 0){
+                    log_mapping(fullVPN, frameNum, -1, false);     // logging new map
+                }
+                frameNum++;                 // incrementing frameNum
+            }
+            else {                          // performing page replacement
+                unsigned int victimVpn = WSClock_entry[clock_hand_position]->vpn;               // getting VPN of the victim page
+                clock_hand_position = page_replacement(WSClock_entry, clock_hand_position, ageRecentLastAccess, numOfAddresses, vAddr >> page_table->numOfBitsOffset, page_table);
+                int replacedFrame = WSClock_entry[clock_hand_position]->frameNum;
+                page_table->numOfPageReplaced++;
+                if (strcmp(logMode, "vpn2pfn_pr") == 0){
+                    log_mapping(fullVPN, replacedFrame, victimVpn, false);
+                }
+                    
+            }
         }
 
-        // moving to next level if exists, else return null
-        if (currLevel->nextLevelPtr[index] == nullptr){         //check if valid mapping found
-            return nullptr;                                     // returning nullptr if not
-        }
-        // if next level exists, go to next level
-        currLevel = currLevel->nextLevelPtr[index];
+        page_table->numOfAddresses++;       // updating total number of address for pagetable
+
+            // if max number of memory accesses is specified (in command line) > 0
+            // and if max number of memory accesses is reached (>= maxNumMemAccesses), then exit while loop
+            if (maxNumMemAccesses > 0 && numOfAddresses >= maxNumMemAccesses) {
+                break;
+            }
     }
-    return nullptr;
-}
 
+
+    // print logs given the log mode from command line
+    if (strcmp(logMode, "bitmasks") == 0){
+        log_bitmasks(page_table->levelCount, page_table->bitMaskAry);
+    }
+
+    else if (strcmp(logMode, "summary") == 0) {
+        log_summary(page_table->pageSize, page_table->numOfPageReplaced, page_table->pageTableHits,
+            page_table->numOfAddresses, page_table->numOfFramesAllocated, page_table->pgTableEntries);
+    }
+
+    // close files before exiting
+    fclose(mtrace_file);            // closing trace file
+    fclose(readswrites_file);       // closing read/write file
+    delete[] WSClock_entry;         // deallocating memory for WSClock entries
+
+    return 0; // exit program, as it successfully complete
+    
+}
